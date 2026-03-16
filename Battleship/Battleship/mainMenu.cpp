4b186b9b698a3828c2555bb4b3da36620a33ee1c
Owen Foggett <Ofoggett5232@conestogac.on.ca>
2026-03-16 18:16:45 -0400

    First Commit : Added Basic Functionality for a main menu

--- Battleship/Battleship/mainMenu.cpp
+++ Battleship/Battleship/mainMenu.cpp
@@ -2,4 +2,37 @@
 #include <string>
 #include "mainMenu.h"
 
+//using namespace std; ?? What do we prefer here?
 //Add your code here
+
+int mainMenu(){
+	bool runProgram;
+	int userNum;
+	while (runProgram) {
+		std::cout << "Welcome to Battleship\n\n";
+		std::cout << "1. Play\n";
+		std::cout << "2. Help\n";
+		std::cout << "3. Saves\n";
+		std::cout << "4. End\n";
+
+		std::cin >> userNum;
+
+		switch (userNum) {
+		case 1:
+			//userInput();
+			//Enter User Input
+		case 2:
+			//helper();
+			//Enter Helper.cpp
+		case 3:
+			//gatherGameData();
+			//Enter File IO List
+		case 4:
+			runProgram = false;
+			break;
+		default:
+			std::cout << "Invalid Input\n";
+			//Command to clear screen here
+		}
+	}
+}
