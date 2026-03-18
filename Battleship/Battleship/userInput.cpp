b8245a4e1691ae04e58642a6b4b699da880ac262
vincentkacperskiGit <kacperskivincent@gmail.com>
2026-03-18 12:07:13 -0400

    Moved class UserData from userInput.cpp into userInput.h for better organization.

--- Battleship/Battleship/userInput.cpp
+++ Battleship/Battleship/userInput.cpp
@@ -5,60 +5,7 @@
 
 //Note: The code below is not tested or finished yet.
 //Do not run the code below as it needs further testing.
-//Testing will begin tomorrow due to delays. 
-
-class UserData {
-private:
-
-	std::string nickname;
-	std::string username;
-	int players = 0;
-	int age = 0;
-
-public:
-
-	//Default constructor
-	UserData() {}
-
-	//Build Constructor
-	UserData(std::string n, std::string u, int a, int p) {
-		username = u;
-		nickname = n;
-		players = p;
-		age = a;
-	}
-
-	//Setter functions
-
-	void storePlayers(int p) { //Store player count
-		players = p;
-	}
-	void storeNickname(std::string n) { //Store nickname
-		nickname = n;
-	}
-	void storeUsername(std::string n) { //Store username
-		username = n;
-	}
-	void storeAge(int a) { //Store age
-		age = a;
-	}
-
-	//Getter functions
-
-	int getPlayers() { //Get player count
-		return players;
-	}
-	std::string getNickname() { //Store nickname
-		return nickname;
-	}
-	std::string getUsername() { //Store username
-	    return username;
-	}
-	int getAge() { //Store age
-		return age;
-	}
-
-};
+//Testing will begin tomorrow due to delays.
 
 namespace data {
 
