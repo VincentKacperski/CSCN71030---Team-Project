71e7a8acfff2f3b9c0e30b34b5e1240ea16bbe3c
Jacob <159152535+JacobFonseca@users.noreply.github.com>
2026-04-09 13:07:50 -0400

    Clean up shipChecking header and connect it to UserData

--- Battleship/Battleship/shipChecking.h
+++ Battleship/Battleship/shipChecking.h
@@ -1,7 +1,12 @@
 #pragma once
-#include "placeShips.h"
-#include "attack.h"
+
 #include "Base.h"
-void ShipChecking(); 
-void placeShips(int x, int y, int orientation, int size);
-void checkHit(int x, int y);
+
+// Initializes the player's own board and tracking board.
+void initializeBoards(UserData& user, int boardSize);
+
+// Places a ship on the player's own board.
+bool placeShips(UserData& user, int x, int y, int orientation, int size, char symbol);
+
+// Checks whether an attack hits a ship on the defender's board.
+bool checkHit(UserData& defender, int x, int y);
